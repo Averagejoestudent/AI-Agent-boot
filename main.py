0aef9648301e614e5dbeb2d6ec5c5da003f5c411
ThePrimeagen <zoimmoiz@gmail.com>
2025-11-01 23:34:14 +0500

    Second commit

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 from google import genai
+load_dotenv()
+#________________________________________________________________#
 
+
+api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-print(api_key)
+
+content = "Why is Boot.dev such great place to learn backend development? Use one paragraph maximum."
+
+
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents=content
+)
+print(response.text)
+
+if response.usage_metadata:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    #print(f"Total Token Count: {response.usage_metadata.total_token_count}")
+else:
+    print("Usage metadata not available in this response.")
