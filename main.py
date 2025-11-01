8f052b7ecd7eb45c92e501a0e351e6d7602c6c2c
ThePrimeagen <zoimmoiz@gmail.com>
2025-11-01 23:56:33 +0500

    Third commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 load_dotenv()
 #________________________________________________________________#
 
@@ -8,18 +9,20 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-content = "Why is Boot.dev such great place to learn backend development? Use one paragraph maximum."
 
-
-
-response = client.models.generate_content(
+if sys.argv[1]:
+    content = sys.argv[1]
+    response = client.models.generate_content(
     model='gemini-2.5-flash', contents=content
-)
-print(response.text)
+    )
+    print(response.text)
 
-if response.usage_metadata:
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    #print(f"Total Token Count: {response.usage_metadata.total_token_count}")
+    if response.usage_metadata:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        #print(f"Total Token Count: {response.usage_metadata.total_token_count}")
+    else:
+        print("Usage metadata not available in this response.")
 else:
-    print("Usage metadata not available in this response.")
+    Exception("No input provided")
+    sys.exit(1)
