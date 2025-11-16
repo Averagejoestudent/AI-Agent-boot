7fd8a4d03c1b1afa6adc0e996195bcb2836e4d95
ThePrimeagen <zoimmoiz@gmail.com>
2025-11-17 00:15:10 +0500

    Eighth commit

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,19 +1,24 @@
 import os
-def get_file_content(working_directory, file_path):
-    abs_working = os.path.abspath(working_directory)
-    abs_filepath = os.path.abspath(os.path.join(working_directory, file_path))
-    check_inside = os.path.exists(abs_filepath)
-    is_file = os.path.isfile(abs_filepath)
-    
-    if not check_inside:
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not is_file:
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    return f"its working"
-    # MAX_CHARS = 10000
+from config import MAX_CHARS
 
-    # with open(abs_target, "r") as f:
-    #     file_content_string = f.read(MAX_CHARS)
-print(get_file_content("calculator", "get_files_info.py"))
-print(get_file_content("calculator", "main.py"))
-print(get_file_content("calculator", "pkg/calculator.py"))
+def get_file_content(working_directory, file_path):
+    try:
+        abs_working = os.path.abspath(working_directory)
+        abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+        check_inside = abs_file_path.startswith(abs_working)
+        is_file = os.path.isfile(abs_file_path)
+    
+        if not check_inside:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not is_file:
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        with open(abs_file_path, "r") as f:
+            file_content = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
+                file_content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+            return file_content
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
--- tests.py
+++ tests.py
@@ -1,22 +1,11 @@
 # python
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-def run():
-    print("Result for current directory:")
-    print(get_files_info("calculator", "."))
-    print()
-
-    print("Result for 'pkg' directory:")
-    print(get_files_info("calculator", "pkg"))
-    print()
-
-    print("Result for '/bin' directory:")
-    print(get_files_info("calculator", "/bin"))
-    print()
-
-    print("Result for '../' directory:")
-    print(get_files_info("calculator", "../"))
-    print()
+def main():
+    print(get_file_content("calculator", "main.py"))
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(get_file_content("calculator", "/bin/cat"))
+    print(get_file_content("calculator", "pkg/does_not_exist.py"))
 
 if __name__ == "__main__":
-    run()
+    main()
