d97509936ec22a436ad28d05a4fd0f1749de2412
ThePrimeagen <zoimmoiz@gmail.com>
2025-12-01 21:59:00 +0500

    Fourteenth commit

---
+++ call_function.py
@@ -0,0 +1,44 @@
+from google.genai import types
+from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
+from functions.write_file import schema_write_file
+
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
+    ]
+)
+
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+            print(f" - Calling function: {function_call_part.name}")
+        
+    try:
+        function_name = function_call_part.name
+        function_result = function_name(**{"working_directory" : "./calculator"})
+        return types.Content(
+        role="tool",
+        parts=[
+        types.Part.from_function_response(
+            name=function_name,
+            response={"result": function_result},
+        )
+    ],
+)
+    except:
+        return types.Content(
+        role="tool",
+        parts=[
+        types.Part.from_function_response(
+            name=function_name,
+            response={"error": f"Unknown function: {function_name}"},
+        )
+        ],
+)
--- main.py
+++ main.py
@@ -3,76 +3,70 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
-import argparse
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
-load_dotenv()
-#________________________________________________________________#
-parser = argparse.ArgumentParser()
-
-parser.add_argument(sys.argv[1],type=str, help="Input string")
-parser.add_argument('-v', '--verbose',
-                    action='store_true')  # on/off flag # 
-
-args = parser.parse_args()
-verbose = args.verbose
-#________________________________________________________________#
+from prompt import system_prompt
+from call_function import available_functions,call_function
 
 
-system_prompt = """
-You are a helpful AI coding agent.
+def main():
 
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+    load_dotenv()
+    
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
-- List files and directories
-- Read file contents
-- Execute Python files with optional arguments
-- Write or overwrite files
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
+        sys.exit(1)
 
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
+    
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
+    user_prompt = " ".join(args)
 
-#________________________________________________________________#
-available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-        schema_get_file_content,
-        schema_run_python_file,
-        schema_write_file
-    ]
-)
-messages = [
-    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
-]
-
-
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-
-
-if sys.argv[1]:
-    response = client.models.generate_content(
-    model='gemini-2.5-flash', 
-    contents=messages,
-    config=types.GenerateContentConfig(
-    tools=[available_functions], system_instruction=system_prompt),
-    )
-    if response.function_calls:
-        for fn in response.function_calls:
-            print(f"Calling function: {fn.name}({fn.args})")
-    else:
-        print(response.text)
     if verbose:
-        if response.usage_metadata:
-            print(f"User prompt: {sys.argv[1]}")
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+
+
+def generate_content(client, messages, verbose):
+        response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=messages,
+        config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt),
+        )
+        
+        if verbose:
             print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-            #print(f"Total Token Count: {response.usage_metadata.total_token_count}")
+        
+        
+        
+        
+        if not response.function_calls:
+            print(response.text)
+        
         else:
-            print("Usage metadata not available in this response.")
-else:
-    Exception("No input provided")
-    sys.exit(1)
+            for fn in response.function_calls:
+                call = call_function(fn)
+                if call.parts[0].function_response.response:
+                    list_to_use_later = []
+                    list_to_use_later.append(call.parts[0].function_response.response['result'])
+                else:
+                    Exception("Fatal exception")
+        
+if __name__ == "__main__":
+    main()
